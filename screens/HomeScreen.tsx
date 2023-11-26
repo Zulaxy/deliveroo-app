f1fbb3d9b19b606145f7e9950ff4858c77f112ba
Galin Malchev <g.malchev@live.com>
2023-11-26 19:38:47 +0200

    1st screen basic setup

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,39 @@
+import React, { useLayoutEffect } from "react";
+import { Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-600 tex-xs">Deliver Now</Text>
+          <View className="flex-row items-center">
+            <Text className="font-bold text-xl">Current Location</Text>
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </View>
+        </View>
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
