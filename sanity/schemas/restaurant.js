208af576869591f45d4e6f38cf06d5a3474ff4ae
Galin Malchev <g.malchev@live.com>
2023-11-30 19:45:48 +0200

    updating restaurant schema

---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,57 @@
+export default {
+  name: "restaurant",
+  title: "Restaurant",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "Latitude",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longtitude",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Address",
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Rating",
+      validation: (rule) =>
+        rule
+          .required()
+          .min(1)
+          .max(5)
+          .error("Please enter a valid rating between 1 and 5."),
+    },
+    {
+      type: "reference",
+      name: "category",
+      title: "Category",
+      validation: (rule) => rule.required(),
+      to: [{ type: "category" }],
+    },
+    {
+      type: "reference",
+      name: "dishes",
+      title: "Dishes",
+      of: [{ type: "reference", to: [{ type: "dishes" }] }],
+    },
+  ],
+};
